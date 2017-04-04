4543bb5e6c45b02695508537005c087d2b06db27
Cédric Brun <cedric.brun@obeo.fr>
2017-04-04 11:41:00 +0200

    Promote the full-update site as part of the build

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env sh
 UPDATE_SITE_DIR=org.obeonetwork.capella.update/target/repository
+UPDATE_SITE_DIR_FULL=org.obeonetwork.capella.update.full/target/repository
 DEPLOY_LOCAL_DIR=$1
 echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create nightly folder
 mkdir $DEPLOY_LOCAL_DIR 
+#the full subdirectory contains the "full" update-site
+mkdir $DEPLOY_LOCAL_DIR/full 
 # Copy update-site and target platform to deploy local dir
 cp -r $UPDATE_SITE_DIR/* $DEPLOY_LOCAL_DIR
-echo "ls ${DEPLOY_LOCAL_DIR}"
-ls $DEPLOY_LOCAL_DIR
+cp -r $UPDATE_SITE_DIR_FULL/* $DEPLOY_LOCAL_DIR/full
+echo "find ${DEPLOY_LOCAL_DIR}"
+find $DEPLOY_LOCAL_DIR
