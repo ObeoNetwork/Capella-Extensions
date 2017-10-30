a255b857aa28de085ccbc19d450606ca9d9db0db
Yvan Lussaud <yvan.lussaud@obeo.fr>
2017-10-30 14:27:39 +0100

    Added parent directory creation to prepare-deploy-local-dir.sh.

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -4,9 +4,9 @@ UPDATE_SITE_DIR_FULL=releng/org.obeonetwork.capella.update.full/target/repositor
 DEPLOY_LOCAL_DIR=$1
 echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create nightly folder
-mkdir $DEPLOY_LOCAL_DIR 
+mkdir -p $DEPLOY_LOCAL_DIR 
 #the full subdirectory contains the "full" update-site
-mkdir $DEPLOY_LOCAL_DIR/full 
+mkdir -p $DEPLOY_LOCAL_DIR/full 
 # Copy update-site and target platform to deploy local dir
 cp -r $UPDATE_SITE_DIR/* $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_DIR_FULL/* $DEPLOY_LOCAL_DIR/full
