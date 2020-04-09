80a4747741c1338607e95932c58ce4585a84c313
DRyzhov <dryzhov@migcredit.ru>
2020-04-09 06:40:16 +0300

    Initial version for M2doc Capella commandline extension

---
+++ plugins/org.obeonetwork.capella.m2doc.commandline/src/org/obeonetwork/capella/m2doc/commandline/M2DocCommandLineActivator.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales - initial API and implementation
+ ******************************************************************************/
+
+package org.obeonetwork.capella.m2doc.commandline;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class M2DocCommandLineActivator extends Plugin {
+	public static final String PLUGIN_ID = "org.obeonetwork.capella.m2doc.commandline"; //$NON-NLS-1$
+
+}
