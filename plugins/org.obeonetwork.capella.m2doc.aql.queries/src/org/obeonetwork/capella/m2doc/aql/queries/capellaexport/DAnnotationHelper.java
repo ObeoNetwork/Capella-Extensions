26315786607722ecd39c99a2946a1026c76d76c5
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-06-01 16:30:56 +0200

    Updated M2DocGenServices.

---
+++ plugins/org.obeonetwork.capella.m2doc.aql.queries/src/org/obeonetwork/capella/m2doc/aql/queries/capellaexport/DAnnotationHelper.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.capella.m2doc.aql.queries.capellaexport;
+
+import org.eclipse.sirius.business.api.helper.SiriusUtil;
+import org.eclipse.sirius.viewpoint.description.DAnnotation;
+import org.eclipse.sirius.viewpoint.description.DModelElement;
+import org.eclipse.sirius.viewpoint.description.DescriptionFactory;
+
+/**
+ * Various helpers for {@link DAnnotation} annotations on {@link DModelElement} elements.
+ */
+public class DAnnotationHelper {
+
+    /**
+     * Create a DAnnotation with a given source on a given model element
+     * 
+     * @param source
+     * @param representation
+     */
+    public static DAnnotation createAnnotation(final String source, DModelElement representation) {
+        DAnnotation annotation = DescriptionFactory.eINSTANCE.createDAnnotation();
+        annotation.setSource(source);
+        representation.getEAnnotations().add(annotation);
+        return annotation;
+    }
+
+    /**
+     * Get the first annotation with a given source from a given model element, optionally creating the annotation if none
+     * previously existed.
+     * 
+     * @param source
+     * @param representation
+     * @param create
+     */
+    public static DAnnotation getAnnotation(String source, DModelElement representation, boolean create) {
+        for (DAnnotation annotation : representation.getEAnnotations()) {
+            if (annotation.getSource() != null && annotation.getSource().equals(source)) {
+                return annotation;
+            }
+        }
+
+        if (create) {
+            return createAnnotation(source, representation);
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns whether the element has the given element
+     * 
+     * @param source
+     * @param representation
+     */
+    public static boolean hasAnnotation(String source, DModelElement representation) {
+        return getAnnotation(source, representation, false) != null;
+    }
+
+    /**
+     * Delete the first annotation with a given source from a given model element
+     * 
+     * @param source
+     * @param representation
+     * @return true if an annotation was deleted, false otherwise
+     */
+    public static boolean deleteAnnotation(String source, DModelElement representation) {
+        DAnnotation annotation = getAnnotation(source, representation, false);
+        if (annotation != null) {
+            SiriusUtil.delete(annotation);
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ plugins/org.obeonetwork.capella.m2doc.aql.queries/src/org/obeonetwork/capella/m2doc/aql/queries/capellaexport/IRepresentationAnnotationConstants.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2019 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.capella.m2doc.aql.queries.capellaexport;
+
+/**
+ * Define DRepresentation annotation names.
+ * 
+ */
+public interface IRepresentationAnnotationConstants {
+
+  /**
+   *
+   */
+  public static final String NotVisibleInDoc = "http://www.polarsys.org/capella/core/NotVisibleInDoc"; //$NON-NLS-1$
+
+  /**
+   *
+   */
+  public static final String NotVisibleInLM = "http://www.polarsys.org/capella/core/NotVisibleInLM"; //$NON-NLS-1$
+
+  /**
+   *
+   */
+  public static final String ProgressStatus = "http://www.polarsys.org/capella/core/ProgressStatus"; //$NON-NLS-1$
+
+  /**
+   *
+   */
+  public static final String StatusReview = "http://www.polarsys.org/capella/core/StatusReview"; //$NON-NLS-1$
+
+  /**
+   * This annotation key is deprecated. Annotation is now stored as a EReference towards the EnumerationPropertyLiteral
+   */
+  @Deprecated
+  public static final String PROGRESS_VALUE_KEYVALUE = "value"; //$NON-NLS-1$
+
+  /**
+   * 
+   */
+  public static final String REVIEW_VALUE_KEYVALUE = "value"; //$NON-NLS-1$
+
+  /**
+   * Key used to store contextual elements
+   */
+  public static final String ContextualElements = "http://www.polarsys.org/capella/dannotation/ContextualElements"; //$NON-NLS-1$
+
+  /**
+   * Used on DRepresentationDescriptors to denote some filters that have been desactivated by the user
+   */
+  public static final String DesactivatedFilters = "https://www.polarsys.org/capella/dannotation/DesactivatedFilters"; //$NON-NLS-1$
+
+}
