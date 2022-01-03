59cf5a6989487b335b1952eaea74b89f1d4d6598
Yvan Lussaud <yvan.lussaud@obeo.fr>
2022-01-03 12:11:40 +0100

    Removed zip check from tests.

--- tests/org.obeonetwork.capella.m2doc.aql.queries.tests/src/org/obeonetwork/capella/aql/queries/tests/IFETEmplateTests.java
+++ tests/org.obeonetwork.capella.m2doc.aql.queries.tests/src/org/obeonetwork/capella/aql/queries/tests/IFETEmplateTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2019 Obeo. 
+ *  Copyright (c) 2019, 2022 Obeo. 
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v2.0
  *  which accompanies this distribution, and is available at
@@ -11,41 +11,14 @@
  *******************************************************************************/
 package org.obeonetwork.capella.aql.queries.tests;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.URIConverter;
-import org.junit.Test;
 import org.junit.runners.Parameterized.Parameters;
 import org.obeonetwork.m2doc.parser.DocumentParserException;
 import org.obeonetwork.m2doc.sirius.tests.AbstractTemplatesTestSuite;
-import org.obeonetwork.m2doc.tests.M2DocTestUtils;
-import org.obeonetwork.m2doc.tests.TestMemoryURIHandler;
 
 public class IFETEmplateTests extends AbstractTemplatesTestSuite {
-
-	private static final Map<String, String> generatedToZipped = initGeneratedToZipped();
-
-	/**
-	 * The buffer size.
-	 */
-	private static final int BUFFER_SIZE = 1024 * 8;
-
-	private final URI templateURI;
-
-	private final URI generatedURI;
-
-	private final String zippedTemplateName;
-
-	private final String zippedGeneratedName;
 
 	/**
 	 * Constructor.
@@ -59,27 +32,6 @@ public class IFETEmplateTests extends AbstractTemplatesTestSuite {
 	 */
 	public IFETEmplateTests(String testFolder) throws IOException, DocumentParserException {
 		super(testFolder);
-		templateURI = getTemplateURI(new File(testFolder));
-		generatedURI = getExpectedGeneratedURI(new File(testFolder));
-		zippedTemplateName = generatedToZipped.get(templateURI.lastSegment());
-		zippedGeneratedName = generatedToZipped.get(generatedURI.lastSegment());
-	}
-
-	/**
-	 * Initializes the mapping from generated to zipped document names.
-	 * 
-	 * @return the mapping from generated to zipped document names
-	 */
-	private static Map<String, String> initGeneratedToZipped() {
-		final Map<String, String> res = new HashMap<String, String>();
-
-		res.put("LA-Complete-expected-generation.docx", "generated/LA_Complete.docx");
-		res.put("LA-Complete-template.docx", "template/Template LA Complete.docx");
-
-		res.put("SA-Complete-expected-generation.docx", "generated/SA_Complete.docx");
-		res.put("SA-Complete-template.docx", "template/Template SA Complete.docx");
-
-		return res;
 	}
 
 	/**
@@ -90,43 +42,6 @@ public class IFETEmplateTests extends AbstractTemplatesTestSuite {
 	@Parameters(name = "{0}")
 	public static Collection<Object[]> retrieveTestFolders() {
 		return retrieveTestFolders("resources/IFE");
-	}
-
-	@Test
-	public void zipTemplateComparison() throws IOException {
-		zipComparison(zippedTemplateName, templateURI);
-	}
-
-	@Test
-	public void zipGeneratedComparison() throws IOException {
-		zipComparison(zippedGeneratedName, generatedURI);
-	}
-
-	protected void zipComparison(String entryName, URI expectedURI) throws IOException {
-		try (final ZipFile zipFile = new ZipFile("../../plugins/org.obeonetwork.capella.m2doc.aql.queries/zips/m2docife.zip");) {
-			final ZipEntry templateEntry = zipFile.getEntry(entryName);
-			final InputStream zippedTemplateInputStream = zipFile.getInputStream(templateEntry);
-			final TestMemoryURIHandler handler = new TestMemoryURIHandler();
-			try {
-				final URI tempURI = URI.createURI(TestMemoryURIHandler.PROTOCOL +"://" + entryName);
-
-				// Copy zippedTemplateInputStream to the given URI
-				URIConverter.INSTANCE.getURIHandlers().add(0, handler);
-				final OutputStream outputStream = URIConverter.INSTANCE.createOutputStream(tempURI);
-				byte[] buffer = new byte[BUFFER_SIZE];
-				int size = zippedTemplateInputStream.read(buffer, 0, buffer.length);
-				while (size != -1) {
-					outputStream.write(buffer, 0, size);
-					size = zippedTemplateInputStream.read(buffer, 0, buffer.length);
-				}
-				outputStream.flush();
-
-				M2DocTestUtils.assertDocx(expectedURI, tempURI);
-			} finally {
-				handler.clear();
-				URIConverter.INSTANCE.getURIHandlers().remove(handler);
-			}
-		}
 	}
 
 }
